53952c23d61d8efdda9a6ea24f9a2091fd9719ba
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-08-16 11:23:17 -0500

    Update myApp.js
    create a model

--- myApp.js
+++ myApp.js
@@ -3,6 +3,13 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI);
 
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: { type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
 
 let Person;
 
