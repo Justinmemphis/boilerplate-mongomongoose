9073873181320ef500266ab189c4d69131667d70
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-08-16 12:58:38 -0500

    Update myApp.js
    chain search query helpers to narrow search results

--- myApp.js
+++ myApp.js
@@ -96,7 +96,14 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  const findQuery = Person.find({ favoriteFoods: foodToSearch })
+                          .sort({ name: 1 })
+                          .limit({ 2 })
+                          .select({ age: 0 })
+                          .exec( (err, people) => {
+                            if(err) return console.log(err);
+                            done(null, personName);
+                          })
 };
 
 /** **Well Done !!**
